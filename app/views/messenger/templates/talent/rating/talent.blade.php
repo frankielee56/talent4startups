0b96feb46844686716338e951098f4c6c73fb446
Jesus Fernandez <jesus.fndz@gmail.com>
2015-02-04 12:14:35 -0500

    Preparations for laravel 5 upgrade
    Signed-off-by: Jesus Fernandez <jesus.fndz@gmail.com>

--- app/views/messenger/templates/talent/rating/talent.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('messenger.templates.base')
-@section('subject')
-You have a new rating
-@endsection
-
-@section('body')
-Check it out on your {{ link_to_route('profile_path', 'Profile', $recipient->id) }}.
-@endsection
